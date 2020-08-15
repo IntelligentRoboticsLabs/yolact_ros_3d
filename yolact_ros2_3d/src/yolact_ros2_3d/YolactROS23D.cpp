75c46ad2f835f0beea9caafbdb2f2ac236b6c606
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-08-15 11:22:25 +0200

    [YolactROS23D] library added. Not finished

---
+++ yolact_ros2_3d/src/yolact_ros2_3d/YolactROS23D.cpp
@@ -0,0 +1,28 @@
+// Copyright 2020 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* Author: Francisco Martín fmrico@gmail.com */
+/* Author: Fernando González fergonzaramos@yahoo.es */
+
+#include "yolact_ros2_3d/YolactROS23D.hpp"
+
+namespace yolact_ros2_3d
+{
+YolactROS23D::YolactROS23D()
+: LifecycleNode("yolact_ros2_3d_node")
+{
+	;
+}
+
+}	// namespace yolact_ros2_3d
