437d4e44ad25cf71d94c5f210715f759531ce125
Francisco Martin Rico <fmrico@gmail.com>
2020-11-11 19:12:08 +0100

    Functional version
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

--- yolact_ros2_3d/include/yolact_ros2_3d/YolactROS23D.hpp
+++ yolact_ros2_3d/include/yolact_ros2_3d/YolactROS23D.hpp
@@ -18,26 +18,39 @@
 #ifndef YOLACT_ROS2_3D__YOLACTROS23D_HPP_
 #define YOLACT_ROS2_3D__YOLACTROS23D_HPP_
 
-#include <tf2/convert.h>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_listener.h>
-#include <cv_bridge/cv_bridge.h>
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp_lifecycle/lifecycle_node.hpp>
-#include <lifecycle_msgs/msg/state.hpp>
-#include <sensor_msgs/msg/point_cloud2.hpp>
-#include <sensor_msgs/msg/point_cloud.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/mat.hpp>
-#include <visualization_msgs/msg/marker_array.hpp>
 #include <string>
 #include <vector>
 #include <map>
+
+#include <tf2/convert.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/mat.hpp>
+
+#include "pcl/kdtree/kdtree_flann.h"
+
+#include "octomap/octomap.h"
+#include "octomap/OcTree.h"
+#include "octomap/OcTreeKey.h"
+
 #include "yolact_ros2_msgs/msg/detections.hpp"
 #include "yolact_ros2_msgs/msg/detection.hpp"
-#include "gb_visual_detection_3d_msgs/msg/bounding_boxes3d.hpp"
-#include "gb_visual_detection_3d_msgs/msg/bounding_box3d.hpp"
+#include "octomap_msgs/msg/octomap.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+
+#include "octomap_msgs/conversions.h"
+#include "octomap_msgs/msg/octomap.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "lifecycle_msgs/msg/state.hpp"
+
 
 namespace yolact_ros2_3d
 {
@@ -45,8 +58,6 @@ class YolactROS23D : public rclcpp_lifecycle::LifecycleNode
 {
 public:
   YolactROS23D();
-
-  void update();
 
 private:
   using CallbackReturnT =
@@ -59,46 +70,54 @@ private:
   CallbackReturnT on_shutdown(const rclcpp_lifecycle::State & state);
   CallbackReturnT on_error(const rclcpp_lifecycle::State & state);
 
-  /* ----------- */
-
-  void pointCloudCb(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
-  void yolactCb(const yolact_ros2_msgs::msg::Detections::SharedPtr msg);
-  void calculate_boxes(
-    const sensor_msgs::msg::PointCloud2 & cloud_pc2,
-    const sensor_msgs::msg::PointCloud & cloud_pc,
-    gb_visual_detection_3d_msgs::msg::BoundingBoxes3d * boxes);
-  void getMask(yolact_ros2_msgs::msg::Detection det, cv::Mat * output_mask);
-  void erodeMask(std::string class_name, cv::Mat * mask, cv::Mat * eroded_mask);
-  void publishMarkers(const gb_visual_detection_3d_msgs::msg::BoundingBoxes3d & boxes);
-
-  bool setErodingFactors();
-  bool pixelBelongsToBbox(const yolact_ros2_msgs::msg::Mask & mask, size_t x, size_t y);
-  bool calculateBbox(
-    const sensor_msgs::msg::PointCloud2 & cloud_pc2,
-    const sensor_msgs::msg::PointCloud & cloud_pc,
-    const yolact_ros2_msgs::msg::Detection & det, const cv::Mat & eroded_mask,
-    gb_visual_detection_3d_msgs::msg::BoundingBox3d * bbox);
-
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr point_cloud_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
   rclcpp::Subscription<yolact_ros2_msgs::msg::Detections>::SharedPtr yolact_ros_sub_;
+  rclcpp_lifecycle::LifecyclePublisher<octomap_msgs::msg::Octomap>::SharedPtr octomaps_pub_;
+  rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::PointCloud2>::SharedPtr detection_cloud_pub;
+  rclcpp::TimerBase::SharedPtr timer_;
 
-  rclcpp_lifecycle::LifecyclePublisher
-  <gb_visual_detection_3d_msgs::msg::BoundingBoxes3d>::SharedPtr yolact3d_pub_;
+  void pointCloud_callback(sensor_msgs::msg::PointCloud2::UniquePtr msg);
+  void image_callback(sensor_msgs::msg::Image::UniquePtr msg);
+  void yolact_callback(yolact_ros2_msgs::msg::Detections::UniquePtr msg);
+  void timer_callback();
 
-  rclcpp_lifecycle::LifecyclePublisher
-  <visualization_msgs::msg::MarkerArray>::SharedPtr markers_pub_;
+  sensor_msgs::msg::PointCloud2::UniquePtr last_pointcloud_ = nullptr;
+  sensor_msgs::msg::Image::UniquePtr last_image_ = nullptr;
+  yolact_ros2_msgs::msg::Detections::UniquePtr last_detection_ = nullptr;
 
-  rclcpp::Clock clock_;
-  tf2_ros::Buffer tfBuffer_;
-  tf2_ros::TransformListener tfListener_;
-  rclcpp::Time last_detection_ts_;
-  sensor_msgs::msg::PointCloud2 orig_point_cloud_;
-  std::string point_cloud_topic_, working_frame_, input_bbx_topic_, output_bbx3d_topic_;
-  std::vector<yolact_ros2_msgs::msg::Detection> original_detections_;
-  std::vector<std::string> interested_classes_ = {};
-  std::map<std::string, int> eroding_factors_;
+  std::string point_cloud_topic_, image_topic_, working_frame_, input_bbx_topic_, output_bbx3d_topic_;
+  std::vector<std::string> interested_classes_;
+  std::vector<std::string> class_colors_;
   double minimum_probability_, maximum_detection_threshold_;
-  bool pc_received_, debug_;
+  double voxel_res_;
+
+  bool timestamps_diff_long(
+    builtin_interfaces::msg::Time & t1, builtin_interfaces::msg::Time & t2,
+    std::chrono::milliseconds threshold);
+
+  void get_octree_from_detection(
+    pcl::KdTreeFLANN<pcl::PointXYZRGB> & kdtree,
+    const yolact_ros2_msgs::msg::Detection & detection,
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud,
+    std::shared_ptr<octomap::ColorOcTree> octree);
+
+  std::shared_ptr<octomap::ColorOcTree> get_initial_octree();
+
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr get_detection_cloud(
+    const yolact_ros2_msgs::msg::Detection & detection,
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud);
+
+  void expand_octree(
+    const octomap::point3d & point,
+    pcl::KdTreeFLANN<pcl::PointXYZRGB> & kdtree,
+    std::shared_ptr<octomap::ColorOcTree> octree);
+
+  void publish_octree(std::shared_ptr<octomap::ColorOcTree> octree,
+    sensor_msgs::msg::PointCloud2::UniquePtr & pointcloud);
+
+  bool pixel_in_detection(const yolact_ros2_msgs::msg::Mask & mask, size_t x, size_t y);
+  pcl::PointXYZRGB get_detection_color(const yolact_ros2_msgs::msg::Detection & detection);
 };
 
 }  // namespace yolact_ros2_3d
